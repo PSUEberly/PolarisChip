1c833a0330ab1adb95d436c1aa9df18a57e3ad4c
Patrick Eberly <patrick.eberly.phone@gmail.com>
2024-09-29 19:33:21 -0400

    Hello

--- src/my-card.js
+++ src/my-card.js
@@ -14,40 +14,115 @@ export class MyCard extends LitElement {
 
   constructor() {
     super();
-    this.tit = '';
-    this.img = '#';
+    this.title = 'My card';
+    this.link = '#';
+    this.image = null;
+    this.artist = 'text';
+    this.description = 'text';
+    this.fancy = false;
   }
 
   static get styles() {
     return css`
       :host {
-        display: inline-flex;
+        display: inline-block;
       }
-      div{
-        font-size: 24px;
-        padding: 16px;
-        margin: 8px;
+
+      div {
+        padding: 8px;
+        width: 300px;
+        border-radius: 4px;
+        text-align: center;
+        background-color: grey;
+        border: 2px solid black;
+      }
+
+      h1 {
+        font-size:24px;
+      }
+
+      img {
+        margin: auto;
+        display: flex;
+        height: 200px;
+        width: 200px;
+      }
+
+      button {
+        margin: 50px;
+        position: relative;
+        background-color: green;
+      }
+
+      a:focus,
+      a:hover {
+        color: yellow;
+      }
+
+      a {
         text-decoration: none;
+        color: white;
       }
-      :host([green]) {
-        color: green;
-        font-family: Arial, sans-serif;
+      :host([fancy]) {
+        display: block;
+        background-color: pink;
+        border: 2px solid fuchsia;
+        box-shadow: 10px 5px 5px red;
+      }
+      details summary {
+        text-align: left;
+        font-size: 20px;
+        padding: 8px 0;
+      }
+
+      details[open] summary {
         font-weight: bold;
       }
-      :host([img]) a{
-        width:200px;
-        height:200px;
-}
+  
+      details div {
+        border: 2px solid black;
+        text-align: left;
+        padding: 8px;
+        height: 70px;
+        overflow: auto;
+      }
     `;
+  }
+  openChanged(e) {
+    console.log(e.newState);
+    if (e.newState === "open") {
+      this.fancy = true;
+    }
+    else {
+      this.fancy = false;
+    }
   }
 
   render() {
-    return html`<div>${this.tit}</div>`;
+    return html`<div>
+      <h1>${this.title}</h1>
+      <hr>
+      <p>${this.artist}</p>
+      <img src=${this.image}>
+      <hr>
+      <details ?open="${this.fancy}" @toggle="${this.openChanged}">
+        <summary>About</summary>
+        
+          ${this.description}
+        
+      </details>
+      <button><a href=${this.link}>Details</a></button>
+      </div>`;
   }
 
   static get properties() {
     return {
-      tit: { type: String },
+      title: { type: String },
+      image: { type: Image},
+      link: {type: String},
+      artist: {type: String},
+      description: {type: String},
+      fancy: { type: Boolean, reflect: true }
     };
   }
 }
